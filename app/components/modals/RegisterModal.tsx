ae6463da42574a7941b22af775deb23b720d1a15
Mark <mark.gowen78@gmail.com>
2023-04-14 15:46:33 -0700

    Configured Heading.tsx

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,6 +1,66 @@
+'use client';
+
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import {
+    FieldValues,
+    SubmitHandler,
+    useForm,
+} from "react-hook-form";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import Modal from "./Modal";
+import Heading from "../Heading";
+
 const RegisterModal = () => {
+    const RegisterModal = useRegisterModal();
+    const [isLoading, setIsLoading] = useState(false);
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FieldValues>({
+        defaultValues: {
+            name: "",
+            email: "",
+            password: "",
+        },
+    });
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        setIsLoading(true);
+
+        axios
+            .post("/api/register", data)
+            .then(() => {
+                RegisterModal.onClose();
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            })
+    }
+
+    const bodyContent = (
+        <div className="flex flex-col gap-4">
+            <Heading />
+        </div>
+    )
+
     return (
-        <div></div>
+        <Modal
+            disabled={isLoading}
+            isOpen={RegisterModal.isOpen}
+            title="Register"
+            actionLabel="Continue"
+            onClose={RegisterModal.onClose}
+            onSubmit={handleSubmit(onSubmit)}
+            body={bodyContent}
+        />
     );
 }
 
