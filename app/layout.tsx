25861e81ab2e8f3b523d9722590b995be406fe38
Mark <mark.gowen78@gmail.com>
2023-04-14 12:40:30 -0700

    Configured and styled Button.tsx, started configuring Modal.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal />
+          <Modal isOpen />
           <Navbar />
         </ClientOnly>
         {children}
