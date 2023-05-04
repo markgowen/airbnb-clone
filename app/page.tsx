0177eec9565c2a6c01141601f5091c9310459fa5
mark <mark.gowen78@gmail.com>
2023-05-04 10:18:42 -0400

    Configured initial ListingCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,53 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import getListings from "./actions/getListings";
+import ListingCard from "./components/listings/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-  return <></>;
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <Container>
+        <div
+          className="
+            grid
+            grid-cols-1
+            gap-8
+            pt-24
+            sm:grid-cols-2
+            md:grid-cols-3
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+        "
+        >
+          {listings.map((listing: any) => {
+            return (
+              <ListingCard
+                currentUser={currentUser}
+                key={listing.id}
+                data={listing}
+              />
+            );
+          })}
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
