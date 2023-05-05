33a299f165d6ca9f8b785ee44326669ec141fb95
mark <mark.gowen78@gmail.com>
2023-05-05 15:12:41 -0400

    Configured left side of individual listing page

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -2,7 +2,7 @@ import getCurrentUser from "@/app/actions/getCurrentUser";
 import getListingById from "@/app/actions/getListingById";
 import ClientOnly from "@/app/components/ClientOnly";
 import EmptyState from "@/app/components/EmptyState";
-import ListingClient from "@/app/listings/[listingId]/ListingClient";
+import ListingClient from "./ListingClient";
 
 interface IParams {
 	listingId?: string;
@@ -10,7 +10,7 @@ interface IParams {
 
 const ListingPage = async ({ params }: { params: IParams }) => {
 	const listing = await getListingById(params);
-	const currentUser = getCurrentUser();
+	const currentUser = await getCurrentUser();
 
 	if (!listing) {
 		return (
